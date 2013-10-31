f6dba64d879a5755db659a0e02b1b0d88e82eb3b
Tim Ambler <tkambler@gmail.com>
2013-10-31 13:41:10 -0500

    Callback only fires once element is in and and is visible.

--- jquery.whenlive.js
+++ jquery.whenlive.js
@@ -47,7 +47,7 @@ $.fn.whenLive = function(fn) {
         // /polyFill
         $.whenLiveLoop = function() {
         	for ( var ek in $.whenLiveElements ) {
-                if ( $.contains(document.documentElement, $.whenLiveElements[ek]['elem'][0]) ) {
+                if ( $.contains(document.documentElement, $.whenLiveElements[ek]['elem'][0]) && $.whenLiveElements[ek]['elem'].is(':visible') ) {
                     // The element exists within the DOM
                     $.whenLiveElements[ek].fn.call($.whenLiveElements[ek].elem);
                     $.whenLiveElements.splice(ek);
