34c0920d4b525429c864ca83fa32f250fc080418
Tim Ambler <tkambler@gmail.com>
2014-10-09 06:49:43 -0500

    Buh-bye, Vouch.

--- express.vouch.js
+++
@@ -1,9 +0,0 @@
-var port = process.argv[2];
-var express = require('express');
-var server = express(); // better instead
-server.configure(function(){
-	server.use('/', express.static(__dirname + '/src'));
-	server.use(express.static(__dirname + '/example'));
-});
-
-server.listen(port);
